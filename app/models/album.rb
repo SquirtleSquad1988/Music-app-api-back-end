841b5c9809435f30a1dce429733c47e0a719c734
SquirtleSquad1988 <alexanderjayrobles0@gmail.com>
2017-02-22 16:38:08 -0500

    update album model
    updated the model controller so that each inputed parameter of album
    must contain at least one character

--- app/models/album.rb
+++ app/models/album.rb
@@ -4,4 +4,5 @@ class Album < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   validates :user, presence: true
+  validates_length_of :album_name, :artist_name, minimum: 1
 end
